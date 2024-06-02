0e8188fb2c9bc3d2859b92fd432a5c65f4e7bcda
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2024-06-03 00:01:45 +0530

    Add files via upload

---
+++ SETPRECISION FUNCTION/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double number = 123.456789;
+    
+    // Set precision to 2 digits after the decimal point, with fixed-point notation
+    std::cout << std::fixed << std::setprecision(2) << number << std::endl;  // Output: 123.46
+
+    return 0;
+}
